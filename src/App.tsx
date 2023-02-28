e2daccd434c8ad4cb8ada7a31d6b4cc7eb465c4f
Daniele Sabre <daniele.sabre@serijakala.com>
2023-02-28 11:28:28 +0100

    Formatted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,104 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from './components/Navbar';
-import { Box, Card, Container, CssBaseline, Grid, PaletteMode, TextField, ThemeProvider, Typography, createTheme } from '@mui/material';
+import {
+    Box,
+    Card,
+    Container,
+    CssBaseline,
+    Grid,
+    PaletteMode,
+    TextField,
+    ThemeProvider,
+    Typography,
+    createTheme
+} from '@mui/material';
 
 export const LS_PALETTE_MODE_KEY = 'paletteMode';
 
 function App() {
-  const [numbers, setNumbers] = useState('');
-  const [total, setTotal] = useState(0);
-  const [theme, setTheme] = useState(getTheme());
+    const [numbers, setNumbers] = useState('');
+    const [total, setTotal] = useState(0);
+    const [theme, setTheme] = useState(getTheme());
 
-  function getCurrentPaletteMode() {
-    return localStorage.getItem(LS_PALETTE_MODE_KEY) || (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-  }
+    function getCurrentPaletteMode() {
+        return (
+            localStorage.getItem(LS_PALETTE_MODE_KEY) ||
+            (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
+        );
+    }
 
-  function getTheme(paletteMode: string = getCurrentPaletteMode()) {
-    return createTheme({
-      palette: {
-        mode: paletteMode as PaletteMode,
-      },
-    });
-  }
+    function getTheme(paletteMode: string = getCurrentPaletteMode()) {
+        return createTheme({
+            palette: {
+                mode: paletteMode as PaletteMode
+            }
+        });
+    }
 
-  function updatePaletteMode(newPaletteMode: string) {
-    setTheme(getTheme(newPaletteMode));
-  }
+    function updatePaletteMode(newPaletteMode: string) {
+        setTheme(getTheme(newPaletteMode));
+    }
 
-  useEffect(() => {
-    const numbersToSum = numbers
-      .split('\n')
-      .map((n) => n.trim())
-      .filter((n) => n !== '')
-      .map((n) => n.split(',').join('.'))
-      .map((n) => n.replace(/[^0-9.]/, '').trim())
-      .map((n) => parseFloat(n))
-      .filter((n) => !isNaN(n))
-      ;
+    useEffect(() => {
+        const numbersToSum = numbers
+            .split('\n')
+            .map((n) => n.trim())
+            .filter((n) => n !== '')
+            .map((n) => n.split(',').join('.'))
+            .map((n) => n.replace(/[^0-9.]/, '').trim())
+            .map((n) => parseFloat(n))
+            .filter((n) => !isNaN(n));
+        let sum = 0;
+        let decimals = 0;
+        numbersToSum.forEach((n) => {
+            const currDecimals = (n.toString().split('.')[1] || '').length;
+            sum += n;
+            decimals = currDecimals > decimals ? currDecimals : decimals;
+        });
 
-    let sum = 0;
-    let decimals = 0;
-    numbersToSum.forEach((n) => {
-      const currDecimals = (n.toString().split('.')[1] || '').length;
-      sum += n;
-      decimals = currDecimals > decimals ? currDecimals : decimals;
-    });
+        decimals = 10 ** decimals;
 
-    decimals = 10 ** decimals;
+        setTotal(Math.round((sum + Number.EPSILON) * decimals) / decimals);
+    }, [numbers]);
 
-    setTotal(Math.round((sum + Number.EPSILON) * decimals) / decimals);
-  }, [numbers]);
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box>
+                <Navbar togglePaletteModeCallback={updatePaletteMode} />
 
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box>
-        <Navbar togglePaletteModeCallback={updatePaletteMode} />
-
-        <Container
-          maxWidth="lg"
-          sx={{ py: 2 }}
-        >
-          <Grid container spacing={4}>
-            <Grid item xs={6}>
-              <TextField
-                label="List of numbers"
-                helperText="Add one number per line"
-                variant='outlined'
-                multiline
-                sx={{ width: 1 }}
-                defaultValue={numbers}
-                onChange={(event) => setNumbers(event.target.value)}
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <Card sx={{
-                p: 2,
-              }}>
-                <Typography component="h2" variant="h6" color="primary" gutterBottom>
-                  Total
-                </Typography>
-                <Typography component="p" variant="h4">
-                  {total}
-                </Typography>
-              </Card>
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
-    </ThemeProvider>
-  );
+                <Container maxWidth='lg' sx={{py: 2}}>
+                    <Grid container spacing={4}>
+                        <Grid item xs={6}>
+                            <TextField
+                                label='List of numbers'
+                                helperText='Add one number per line'
+                                variant='outlined'
+                                multiline
+                                sx={{width: 1}}
+                                defaultValue={numbers}
+                                onChange={(event) => setNumbers(event.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Card
+                                sx={{
+                                    p: 2
+                                }}
+                            >
+                                <Typography component='h2' variant='h6' color='primary' gutterBottom>
+                                    Total
+                                </Typography>
+                                <Typography component='p' variant='h4'>
+                                    {total}
+                                </Typography>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,43 +1,43 @@
-import React, { FC, useState } from 'react';
-import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
-import { DarkMode, LightMode } from '@mui/icons-material';
-import { LS_PALETTE_MODE_KEY } from '../../App';
+import React, {FC, useState} from 'react';
+import {AppBar, Box, IconButton, Toolbar, Typography} from '@mui/material';
+import {DarkMode, LightMode} from '@mui/icons-material';
+import {LS_PALETTE_MODE_KEY} from '../../App';
 
 interface NavbarProps {
-  togglePaletteModeCallback(newPaletteMode: string): void;
+    togglePaletteModeCallback(newPaletteMode: string): void;
 }
 
 const Navbar: FC<NavbarProps> = (props) => {
-  const [paletteMode, setPaletteMode] = useState(localStorage.getItem(LS_PALETTE_MODE_KEY) || 'light');
+    const [paletteMode, setPaletteMode] = useState(localStorage.getItem(LS_PALETTE_MODE_KEY) || 'light');
 
-  function togglePaletteMode() {
-    const newPaletteMode = paletteMode === 'light' ? 'dark' : 'light';
+    function togglePaletteMode() {
+        const newPaletteMode = paletteMode === 'light' ? 'dark' : 'light';
 
-    setPaletteMode(newPaletteMode);
-    localStorage.setItem(LS_PALETTE_MODE_KEY, newPaletteMode);
-    props.togglePaletteModeCallback(newPaletteMode);
-  }
+        setPaletteMode(newPaletteMode);
+        localStorage.setItem(LS_PALETTE_MODE_KEY, newPaletteMode);
+        props.togglePaletteModeCallback(newPaletteMode);
+    }
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Number sum calculator
-          </Typography>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            color="inherit"
-            onClick={togglePaletteMode}
-          >
-            {paletteMode === 'light' && <DarkMode />}
-            {paletteMode === 'dark' && <LightMode />}
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position='static'>
+                <Toolbar>
+                    <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
+                        Number sum calculator
+                    </Typography>
+                    <IconButton
+                        size='large'
+                        aria-label='account of current user'
+                        color='inherit'
+                        onClick={togglePaletteMode}
+                    >
+                        {paletteMode === 'light' && <DarkMode />}
+                        {paletteMode === 'dark' && <LightMode />}
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 };
 
 export default Navbar;
